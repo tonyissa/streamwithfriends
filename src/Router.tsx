4ce4344975fda85345df078396f0ce34667eee9f
tonyissa96 <52683702+tonyissa@users.noreply.github.com>
2025-08-20 15:39:18 -0400

    Initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './App.css'
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}
